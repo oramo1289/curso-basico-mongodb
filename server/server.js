9327925f2738c4b9fc6df70d8c87b0b026930005
Oscar Ramo <oscar.ramo1289@gmail.com>
2017-02-21 13:28:06 -0600

    third try

--- server/server.js
+++ server/server.js
@@ -1,10 +1,4 @@
-/*
-// var env = process.env.NODE_ENV;
-// console.log('ENV ********', env);
-//$env:NODE_ENV="production"
-// SET \"NODE_ENV=development\"&&
-//SET \"NODE_ENV=test\" &&
-*/
+
 const _ = require('lodash');
 const express = require('express');
 const {ObjectID} = require('mongodb');
